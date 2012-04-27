2962c18e189915a61fcc9c035f22a7c7f80a786a
Ian Brown <igb@hccp.org>
2012-04-26 23:11:37 -0700

    basic bar replacement with score working

---
+++ post.js
@@ -0,0 +1,32 @@
+function replaceInfluencerBarWithNumber()
+{
+    var elems = document.getElementsByTagName('*'),i;
+    for (i in elems)
+        {
+	    if((" "+elems[i].className+" ").indexOf(" influence-bar ") > -1)
+		{
+		    var children = elems[i].childNodes;
+		    for (j in children)
+			{
+			    if((" "+children[j].className+" ").indexOf(" marker ") > -1) {
+				var marker = children[j];
+				var parentNode=elems[i].parentNode;
+				parentNode.removeChild(elems[i]);
+				var score=document.createElement("div");
+				score.setAttribute("class", "influenceNumber");
+				score.setAttribute("style", "color:#000000");
+				//alert(elems[i].id);
+				score.innerHTML="Score: " + marker.style.left.substring(0, 5);
+				parentNode.appendChild(score);
+			    }
+			
+			}
+
+		}
+        }
+}
+
+
+replaceInfluencerBarWithNumber();
+
+
